db9197278f57eac2530d28591ce5d9918087b281
crianbra <crianbra@espol.edu.ec>
2018-01-24 08:54:17 -0500

    Commit pruebas

--- prueba.php
+++ prueba.php
@@ -1,7 +1,10 @@
 <?php
 require_once("Demo.php");
+$id = "1";
 $nombre = "dani";
 $test = new Demo();
+$test->setIdDemo($id);
 $test->setNombre($nombre);
+echo "el id es: ". $test->getIdDemo();
 echo "el nombre es: ". $test->getNombre();
 ?>
